c9c5745172a284b21162fe4ee0f6a778a2f31080
papanugget <evilgenius04@gmail.com>
2018-11-28 20:03:49 -0500

    animated background

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
-// consts
+// global consts
 const canvas = document.getElementById('game-canvas'),
     context = canvas.getContext('2d'),
+    LEFT = 1,
+    RIGHT = 2,
+
+    BACKGROUND_VELOCITY = 42,
+    PAUSED_CHECK_INTERVAL = 200,
+
+    // platform consts
     PLATFORM_HEIGHT = 8,
     PLATFORM_STROKE_WIDTH = 2,
     PLATFORM_STROKE_STYLE = 'rgb(0,0,0)',
+    
+    // platform scrolling offset = PLATFORM_VELOCITY_MULTIPLIER * backgroundOffset
+    PLATFORM_VELOCITY_MULTIPLIER = 4.35,
+    RUNNER_HEIGHT = 43,
+    // starting animation speeds
+    STARTING_BACKGROUND_VELOCITY = 0,
+    STARTING_PLATFORM_OFFSET = 0,
+    STARTING_BACKGROUND_OFFSET = 0,
 
+    // runner starting consts
     STARTING_RUNNER_LEFT = 50,
     STARTING_RUNNER_TRACK = 1,
 
-// track baselines
+    // track baselines
     TRACK_1_BASELINE = 323,
     TRACK_2_BASELINE = 223,
     TRACK_3_BASELINE = 123,
 
-// images 
+    // fps indicator
+    fpsElement = document.getElementById('fps'),
+
+    // images 
     background = new Image(),
     runnerImage = new Image(),
 
-// platforms
+    // runner track
+    runnerTrack = STARTING_RUNNER_TRACK,  
+
+    // platform data
     platformData = [
+        // screen 1
         {
             left: 10,
             width: 230,
@@ -55,20 +78,129 @@ const canvas = document.getElementById('game-canvas'),
             track: 1,
             pulsate: false,
         },
+        // screen 2
+        {   
+            left: 810,
+            width: 100,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(200,200,0)',
+            opacity: 1.0,
+            track: 2,
+            pulsate: false,
+        },
+        {   
+            left: 1025,
+            width: 100,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(80,140,230)',
+            opacity: 1.0,
+            track: 2,
+            pulsate: false,
+        },
+        {   
+            left: 1200,
+            width: 125,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'aqua',
+            opacity: 1.0,
+            track: 3,
+            pulsate: false,
+        },
+        {   
+            left: 1400,
+            width: 180,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(80,140,230)',
+            opacity: 1.0,
+            track: 1,
+            pulsate: false,
+        },
+        // screen 3
+        {   
+            left: 1625,
+            width: 100,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(200,200,0)',
+            opacity: 1.0,
+            track: 2,
+            pulsate: false,
+        },
+        {   
+            left: 1800,
+            width: 250,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(80,140,230)',
+            opacity: 1.0,
+            track: 1,
+            pulsate: false,
+        },
+        {   
+            left: 2000,
+            width: 100,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'rgb(200,200,80)',
+            opacity: 1.0,
+            track: 2,
+            pulsate: false,
+        },
+        {   
+            left: 2100,
+            width: 100,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'aqua',
+            opacity: 1.0,
+            track: 3,
+        },
+        // screen 4
+        {   
+            left: 2269,
+            width: 200,
+            height: PLATFORM_HEIGHT,
+            fillStyle: 'gold',
+            opacity: 1.0,
+            track: 1,
+        },
+        {   
+            left: 2500,
+            width: 200,
+            height: PLATFORM_HEIGHT,
+            fillStyle: '#2b950a',
+            opacity: 1.0,
+            track: 2,
+            pulsate: true,
+        }
     ];
+// time
+let lastAnimationFrameTime = 0,
+    lastFpsUpdateTime = 0,
+    fps = 60,
+    platformVelocity,
+    // velocities
+    bgVelocity = STARTING_BACKGROUND_VELOCITY,
+    // translation offsets 
+    backgroundOffset = STARTING_BACKGROUND_OFFSET,
+    platformOffset = STARTING_PLATFORM_OFFSET;
 
-// init
-function initImgs() {
-    background.src = '../images/background_level_one_dark_red.png';
-    runnerImage.src = '../images/runner.png';
+// functions 
+function drawBackground() {
+    // scroll bg
+    context.translate(-backgroundOffset, 0);
+    // init onscreen
+    context.drawImage(background, 0, 0);
+    // init offscreeen
+    context.drawImage(background, background.width, 0);
 
-    background.onload = function(e) {
-        startGame();
-    };
+    context.translate(backgroundOffset, 0);
 }
 
-function drawBackground() {
-    context.drawImage(background, 0, 0);
+function calculateFps(now) {
+    let fps = 1000 / (now - lastAnimationFrameTime);
+    lastAnimationFrameTime = now;
+    if(now - lastFpsUpdateTime > 1000) {
+        lastFpsUpdateTime = now;
+        fpsElement.innerHTML = fps.toFixed(0) + ' fps';
+    }
+    return fps;
 }
 
 function calcPlatformTop(track) {
@@ -83,12 +215,20 @@ function calcPlatformTop(track) {
     return top;
 }
 
+function turnLeft() {
+    bgVelocity = -BACKGROUND_VELOCITY;
+}
+
+function turnRight() {
+    bgVelocity = BACKGROUND_VELOCITY;
+}
+
 function drawPlatforms() {
     let pd, 
         top;
     
     context.save(); // save context attributes
-
+    // iterate thru platformdata and draw / create a platform using each objects properties
     for(let i = 0; i < platformData.length; i++) {
         pd = platformData[i];
         top = calcPlatformTop(pd.track);
@@ -99,26 +239,75 @@ function drawPlatforms() {
         context.globalAlpha = pd.opacity;
 
         // if you switch the order of the following, you get different fx
-        context.strokeRect(pd.left, top, pd.width, pd.height);
         context.fillRect(pd.left, top, pd.width, pd.height);
+        context.strokeRect(pd.left, top, pd.width, pd.height);
     }
     // restore context attributes
     context.restore();
 }
 
 function drawRunner() {
-    context.drawImage(runnerImage, STARTING_RUNNER_LEFT, calcPlatformTop(STARTING_RUNNER_TRACK) - runnerImage.height);
+    context.drawImage(runnerImage, // image
+                    STARTING_RUNNER_LEFT, // canvas left
+                    calcPlatformTop(STARTING_RUNNER_TRACK) - runnerImage.height); // camvas top
 }
 
 function draw() {
+    setPlatformVelocity();
+    setOffsets();
     drawBackground();
     drawPlatforms();
     drawRunner();
 }
 
-function startGame() {
+function setPlatformVelocity() {
+    platformVelocity = bgVelocity * PLATFORM_VELOCITY_MULTIPLIER;
+}
+
+function setOffsets() {
+    setBackgroundOffset();
+    setPlatformOffset();
+}
+
+function setBackgroundOffset() {
+    let offset = backgroundOffset + bgVelocity / fps;
+    offset > 0 && offset < background.width ? backgroundOffset = offset : backgroundOffset = 0;
+}
+
+function setPlatformOffset() {
+    platformOffset += platformVelocity / fps;
+    if(platformOffset > 2 * background.width) {
+        turnLeft();
+    } else if(platformOffset < 0) {
+        turnRight();
+    }
+}
+
+// animation
+function animate(now) {
+    fps = calculateFps(now);
     draw();
+    requestAnimationFrame(animate);
+}
+
+// init
+function initImgs() {
+    background.src = '../images/background_level_one_dark_red.png';
+    runnerImage.src = '../images/runner.png';
+
+    background.onload = function(e) {
+        startGame();
+    };
+}
+
+function startGame() {
+    // draw();
+    window.requestAnimationFrame(animate);
 }
 
 // launch game
 initImgs();
+
+setTimeout(e => {
+    turnRight();
+}, 1000);
